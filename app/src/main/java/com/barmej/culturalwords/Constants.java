f31bc94e566a6b55f4f76d0748792d537e0fe7ce
AnasElgarhy <44965145+Anas-Elgarhy@users.noreply.github.com>
2021-09-22 21:46:34 +0200

    Add constants clss

---
+++ app/src/main/java/com/barmej/culturalwords/Constants.java
@@ -0,0 +1,6 @@
+package com.barmej.culturalwords;
+
+public class Constants {
+    public static final String INDEX_IMAGE_EXTRA = "index_image";
+    public static final String IMAGE_ID_EXTRA = "image_id_extra";
+}
